303c5a11c646d39a7395e4be0101e0a41855c2df
Ilay <101355829+ilay66@users.noreply.github.com>
2023-05-12 11:20:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ bot = telebot.TeleBot('token from @BotFather')
 
 @bot.message_handler(commands = ['start']) 
 def main(message): 
-    bot.send_message(message.chat.id, f"Hello {message.from_user.first_name}, you used TG bot from github!"
+    bot.send_message(message.chat.id, f"Hello {message.from_user.first_name}, you used TG bot from github!")
+bot.polling(none_stop=True)
