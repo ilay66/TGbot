4ea622659c3ba8f5108df89ab335e4d1addf1c05
Ilay <101355829+ilay66@users.noreply.github.com>
2023-05-12 11:11:24 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import telebot 
+
+bot = telebot.TeleBot('token from @BotFather') 
+
+@bot.message_handler(commands = ['start']) 
+def main(message): 
+    bot.send_message(message.chat.id, f"Hello {message.from_user.first_name}, you used TG bot from github!"
